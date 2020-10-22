a1c2ea905d0953d1b297042e77ee05a0daa095dd
Amruta Madkar <54406187+Amruta-Madkar@users.noreply.github.com>
2020-10-22 12:23:54 +0530

    code update

---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './Nav.css';
+import { Layout, Menu, Typography } from 'antd';
+import 'antd/dist/antd.css';
+import 'antd/dist/antd.css';
+//import './index.css';
+import { Input } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
+
+const { Header} = Layout;
+const { Text } = Typography;
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+const onSearch = value => console.log(value);
+
+function  Nav (){
+    return(
+        <>
+             <Header className="headerStyle">
+                <Text style={{color:'#FFF'}}> Beans Love Beers</Text>
+                <Menu style={{float:'right'}}  mode="horizontal" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">Home</Menu.Item>
+                    <Menu.Item key="2">Favourite</Menu.Item>
+                </Menu>
+            </Header>
+            <Search className='searchbar' placeholder="Search for Beer" enterButton="Search" size="large" onSearch={onSearch} />
+
+            
+
+        </>
+    );
+}
+
+
+
+export default Nav;
